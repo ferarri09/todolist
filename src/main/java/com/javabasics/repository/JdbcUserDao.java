03cd392d9e276cec5eba7c37c09bc9ed48a44575
ferarri09 <docferarri09@gmail.com>
2019-08-03 17:58:09 +0300

    qq

--- src/main/java/com/javabasics/repository/JdbcUserDao.java
+++ src/main/java/com/javabasics/repository/JdbcUserDao.java
@@ -3,21 +3,35 @@ package com.javabasics.repository;
 import com.javabasics.connection.ConnectionFactory;
 import com.javabasics.repository.Entity.UserEntity;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
-public class JdbcUserDao implements UserDao{
-    private Connection connection= ConnectionFactory.getConnection();
+public class JdbcUserDao implements UserDao {
+    private Connection connection = ConnectionFactory.getConnection();
+
     @Override
     public Long save(UserEntity userEntity) {
+        ResultSet rs = null;
+        PreparedStatement statement = null;
         try {
-            Statement statement=connection.createStatement();
-            statement.execute(String.format("insert into user(name) values('%s')",userEntity.name));
+            statement = connection.prepareStatement("insert into user(name) values(?)");
+            statement.setString(1, userEntity.name);
+            //statement.executeUpdate(String.format("insert into user(name) values('%s')", userEntity.name));
+            // select LAST_INSERT_ID()
+            rs = statement.executeQuery("select LAST_INSERT_ID() as id");
+            if (rs.next()) {
+                return rs.getLong("id");
+            }
+            return null;
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Error has occurred during saving");
+        } finally {
+            try {
+                statement.close();
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
-        return null;
     }
 
     @Override
--- src/test/java/com/javabasics/service/UserServiceImplTest.java
+++ src/test/java/com/javabasics/service/UserServiceImplTest.java
@@ -1,18 +1,20 @@
 package com.javabasics.service;
 
+import com.javabasics.repository.Entity.UserEntity;
+import com.javabasics.repository.JdbcUserDao;
 import com.javabasics.repository.UserDao;
 import com.javabasics.service.Model.User;
 import com.javabasics.service.user.UserService;
 import com.javabasics.service.user.UserServiceImpl;
 import org.junit.Test;
-import static junit.framework.TestCase.assertEquals;
-import static junit.framework.TestCase.assertTrue;
+import static junit.framework.TestCase.*;
 
 public class UserServiceImplTest {
-    private UserDao userDao=new InMemoryUserDao();
+    private UserDao userDao=new JdbcUserDao();
     private UserService userService=new UserServiceImpl(userDao);
+    private UserEntity userEntity=new UserEntity();
     @Test
-    public void findTest()
+    public void idIsNotNullAfterSaving()
     {
         User user=new User();
         user.name="John";
